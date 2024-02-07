72f4417a15f733c76ad8860ae8f0be36dbc4de37
chavilla <jcharris.villa@gmail.com>
2024-02-07 09:58:02 -0500

    reversing

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
+<<<<<<< HEAD
 import PersonModel from "./Person/PersonModel";
 
 let personObj = new PersonModel('Jesus', 'Charris')
 console.log(personObj);
+=======
+let firstName = "Jesus";
+console.log("El nombre es ", firstName);
+>>>>>>> parent of 327b5c7 (adding model person)
