872aa9bcdad00948b1ffb038daed3ff90aac78c8
chavilla <jcharris.villa@gmail.com>
2024-02-07 10:00:02 -0500

    main push

--- index.js
+++ index.js
@@ -1,9 +1,4 @@
-<<<<<<< HEAD
 import PersonModel from "./Person/PersonModel";
 
 let personObj = new PersonModel('Jesus', 'Charris')
 console.log(personObj);
-=======
-let firstName = "Jesus";
-console.log("El nombre es ", firstName);
->>>>>>> parent of 327b5c7 (adding model person)
